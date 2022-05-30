b6d608c7fe299ee854718183fa97e740456748fc
Patrick Fournier <p_fournier@hushmail.com>
2022-05-30 15:18:31 -0400

    Reference link point to correct slide.

--- dist/perso.js
+++ dist/perso.js
@@ -7,9 +7,38 @@ function formatTitleSlides(obj) {
 
 formatTitleSlides(document);
 
-$(document).ready(function() {
+$(function() {
+
+  // Tag slides in "References" section with the first and last reference they
+  // contain.
+  let refslide_counter = 0;
+  for (let refslide of ($("#references > section:not(:first-child)"))) {
+    const reflist = $("ol", refslide);
+
+    const startidx = reflist.attr("start") === undefined ?
+          1 : +reflist.attr("start");
+    const lastidx = $("li", reflist).length + startidx - 1;
+
+    $(refslide).attr("first-ref", startidx);
+    $(refslide).attr("last-ref", lastidx);
+    $(refslide).attr("id", "references-" + refslide_counter++);
+  }
 
   // Add link to references.
-  $(".reference").wrapInner("<a href=#/references></a>");
+  for (let ref of $(".reference")) {
+    const refidx = $(ref).text();
+    $(ref).wrapInner(`<a role="button" onclick="gotoref(${refidx})"></a>`);
+  }
 
 });
+
+// Got to the slide containing the reference.
+function gotoref(refidx) {
+  for (let refslide of $("#references > section:not(:first-child)")) {
+    if (refidx >= $(refslide).attr("first-ref"))
+      if (refidx <= $(refslide).attr("last-ref")) {
+        window.location = `#/${$(refslide).attr("id")}`
+        return;
+      }
+  }
+}
